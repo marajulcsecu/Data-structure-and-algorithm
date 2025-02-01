fd64206c4eb0e11bf3de87c803bdd4d2d80d0397
marajulcsecu <marajul.cu@gmail.com>
2025-02-01 07:10:28 +0600

    just modified for practice

---
+++ .idea/Stacks/Main.java
@@ -0,0 +1,13 @@
+import java.util.Stack;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        
+        stack.push("MineCraft");
+        stack.push("pubg");
+        System.out.println(stack);
+    }
+
+}
