ba8487c397965ba9bd5f51e9f03b82cf6a283368
marajulcsecu <marajul.cu@gmail.com>
2025-02-06 21:50:48 +0600

    just modified for practice

---
+++ dynamicArray/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        DynamicArray dynamicarray = new DynamicArray();
+      dynamicarray.add("A");
+      dynamicarray.add("B");
+
+        System.out.println(dynamicarray);
+
+        System.out.println("empty: "+dynamicarray.isEmpty());
+
+    }
+
+}
